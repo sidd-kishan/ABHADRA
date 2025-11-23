849a9a6aa12d52e5c9ce12a52e1a593586915323
Aayush Deo <aayush.deo1994@gmail.com>
2025-11-23 22:39:23 +0530

    Add MicroPython testbench for PIO blinking

---
+++ micropython_testbench.py
@@ -0,0 +1,39 @@
+from rp2 import PIO, StateMachine, asm_pio
+from machine import Pin
+import time
+
+# Import the PIO program (assuming it's in the same file or imported)
+# from blink import blink # If blink() is in a separate file
+
+# Define the PIO program (as above)
+@asm_pio(set_init=PIO.OUT_LOW)
+def blink():
+    wrap_target()
+    pull()
+    mov(x, osr)
+    set(pins, 1)[x]
+    set(pins, 0)[x]
+    in_(osr,16)
+    push()
+    wrap()
+
+# Initialize a state machine on PIO 0, state machine 0
+# with the blink program, on Pin 25 (onboard LED)
+sm = StateMachine(0, blink, freq=2000, set_base=Pin(25))
+
+# Start the state machine
+sm.active(1)
+
+# Send data to the PIO's TX FIFO
+# The PIO program will pull these values and use them as delay counts
+sm.put(500) # Fast blink
+time.sleep(1)
+print(sm.get())
+sm.put(100) # Slower blink
+time.sleep(1)
+print(sm.get())
+sm.put(1000) # Even slower blink
+time.sleep(1)
+print(sm.get())
+# Stop the state machine
+sm.active(0)
