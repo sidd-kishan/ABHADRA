c2068b6b788f29405baf19ce1dcce738785d4ab4
Aayush Deo <aayush.deo1994@gmail.com>
2025-11-26 14:10:24 +0530

    Refactor PIO blink program and state machine setup
    >>> %Run -c $EDITOR_CONTENT
    
    MPY: soft reboot
    1
    0
    4
    5
    6
    1
    7
    1
    8
    1
    1
    1
    1
    >>>

--- micropython_testbench.py
+++ micropython_testbench.py
@@ -1,39 +1,68 @@
-from rp2 import PIO, StateMachine, asm_pio
+import rp2
 from machine import Pin
-import time
 
-# Import the PIO program (assuming it's in the same file or imported)
-# from blink import blink # If blink() is in a separate file
-
-# Define the PIO program (as above)
-@asm_pio(set_init=PIO.OUT_LOW)
+# Define the blink program.  It has one GPIO to bind to on the set instruction, which is an output pin.
+# Use lots of delays to make the blinking visible by eye.
+@rp2.asm_pio(set_init=rp2.PIO.OUT_LOW)
 def blink():
+    set(x,1)           #00
+    jmp("main")        #01
+    set(x,2)		   #02
+    jmp("main")		   #03
+    set(x,3)		   #04
+    jmp("main")		   #05
+    set(x,4)		   #06
+    jmp("main")		   #07
+    set(x,5)		   #08
+    jmp("main")		   #09
+    set(x,6)		   #0a
+    jmp("main")		   #0b
+    set(x,7)		   #0c
+    jmp("main")		   #0d
+    set(x,8)		   #0e
+    jmp("main")		   #0f
     wrap_target()
     pull()
-    mov(x, osr)
-    set(pins, 1)[x]
-    set(pins, 0)[x]
-    in_(osr,32)
+    mov(pc,invert(osr))
+    out(null,4)
+    label("main")
+    in_(x,32)
     push()
     wrap()
 
-# Initialize a state machine on PIO 0, state machine 0
-# with the blink program, on Pin 25 (onboard LED)
-sm = StateMachine(0, blink, freq=2000, set_base=Pin(25))
+# Instantiate a state machine with the blink program, at 2000Hz, with set bound to Pin(25) (LED on the Pico board)
+sm = rp2.StateMachine(0, blink, freq=2000, set_base=Pin(25))
 
-# Start the state machine
+# Run the state machine for 3 seconds.  The LED should blink.
 sm.active(1)
-
-# Send data to the PIO's TX FIFO
-# The PIO program will pull these values and use them as delay counts
-sm.put(500000000) # Fast blink
-time.sleep(1)
+print(sm.get()) # gives 1 as output as the program executes from the top and set 1 in the queue
+sm.put(0x00000000)
 print(sm.get())
-sm.put(40000000000) # Slower blink
-time.sleep(1)
+sm.put(0xeeeeeeee)
 print(sm.get())
-sm.put(1000000) # Even slower blink
-time.sleep(1)
+#sm.put(0xdddddddd) d's are crashing
+#print(sm.get())
+sm.put(0xcccccccc)
 print(sm.get())
-# Stop the state machine
+#sm.put(0xbbbbbbbb) b's are crashing
+#print(sm.get())
+sm.put(0xaaaaaaaa)
+print(sm.get())
+sm.put(0x99999999)
+print(sm.get())
+sm.put(0x88888888)
+print(sm.get())
+sm.put(0x77777777)
+print(sm.get())
+sm.put(0x66666666)
+print(sm.get())
+sm.put(0x55555555)
+print(sm.get())
+#sm.put(0x44444444) 4s are crashing it
+sm.put(0x33333333)
+print(sm.get())
+sm.put(0x22222222)
+print(sm.get())
+sm.put(0x11111111)
+print(sm.get())
 sm.active(0)
