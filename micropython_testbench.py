23eae47e7655da56910b666e520dff6bbef1cb28
Aayush Deo <aayush.deo1994@gmail.com>
2025-11-27 13:20:19 +0530

    Refactor blink function and update state machine
    Refactor blink function to use in_ instruction and update state machine configuration.
    
    Output:-
    
    >>> %Run -c $EDITOR_CONTENT
    
    MPY: soft reboot
    0x12345678
    0x345678
    0x45678
    0x45678
    0x5678
    0x5678
    0x678
    0x678
    0x78
    0x78
    0x8
    0x8
    0x0
    0x0

--- micropython_testbench.py
+++ micropython_testbench.py
@@ -6,63 +6,68 @@ from machine import Pin
 @rp2.asm_pio(set_init=rp2.PIO.OUT_LOW)
 def blink():
     set(x,1)           #00
-    jmp("main")        #01
+    in_(x,4)           #01
     set(x,2)		   #02
-    jmp("main")		   #03
+    in_(x,4)		   #03
     set(x,3)		   #04
-    jmp("main")		   #05
+    in_(x,4)		   #05
     set(x,4)		   #06
-    jmp("main")		   #07
+    in_(x,4)		   #07
     set(x,5)		   #08
-    jmp("main")		   #09
+    in_(x,4)		   #09
     set(x,6)		   #0a
-    jmp("main")		   #0b
+    in_(x,4)		   #0b
     set(x,7)		   #0c
-    jmp("main")		   #0d
+    in_(x,4)		   #0d
     set(x,8)		   #0e
-    jmp("main")		   #0f
+    in_(x,4)		   #0f
+    push()
+    set(x,0)
+    #out(null,4)
+    #in_(osr,28)
+    #push()
     wrap_target()
     pull()
-    mov(pc,invert(osr))
-    out(null,4)
-    label("main")
-    in_(x,32)
-    push()
+    mov(pc,osr)
     wrap()
 
 # Instantiate a state machine with the blink program, at 2000Hz, with set bound to Pin(25) (LED on the Pico board)
-sm = rp2.StateMachine(0, blink, freq=2000, set_base=Pin(25))
+sm = rp2.StateMachine(0, blink, freq=2000, set_base=Pin(25),out_shiftdir=rp2.PIO.SHIFT_RIGHT)
 
 # Run the state machine for 3 seconds.  The LED should blink.
 sm.active(1)
-print(sm.get()) # gives 1 as output as the program executes from the top and set 1 in the queue
-sm.put(0x00000000)
-print(sm.get())
-sm.put(0xeeeeeeee)
-print(sm.get())
-#sm.put(0xdddddddd) d's are crashing
-#print(sm.get())
-sm.put(0xcccccccc)
-print(sm.get())
-#sm.put(0xbbbbbbbb) b's are crashing
-#print(sm.get())
-sm.put(0xaaaaaaaa)
-print(sm.get())
-sm.put(0x99999999)
-print(sm.get())
-sm.put(0x88888888)
-print(sm.get())
-sm.put(0x77777777)
-print(sm.get())
-sm.put(0x66666666)
-print(sm.get())
-sm.put(0x55555555)
-print(sm.get())
-#sm.put(0x44444444) 4s are crashing it
-sm.put(0x33333333)
-print(sm.get())
-sm.put(0x22222222)
-print(sm.get())
-sm.put(0x11111111)
-print(sm.get())
+print(hex(sm.get())) # gives 1 as output as the program executes from the top and set 1 in the queue
+sm.put(0xfffffff0)
+print(hex(sm.get()))
+sm.put(0xfffffff1)
+print(hex(sm.get()))
+sm.put(0xfffffff2)
+print(hex(sm.get()))
+sm.put(0xfffffff3)
+print(hex(sm.get()))
+sm.put(0xfffffff4)
+print(hex(sm.get()))
+sm.put(0xfffffff5)
+print(hex(sm.get()))
+sm.put(0xfffffff6)
+print(hex(sm.get()))
+sm.put(0xfffffff7)
+print(hex(sm.get()))
+sm.put(0xfffffff8)
+print(hex(sm.get()))
+sm.put(0xfffffff9)
+print(hex(sm.get()))
+sm.put(0xfffffffa)
+print(hex(sm.get()))
+sm.put(0xfffffffb)
+print(hex(sm.get()))
+sm.put(0xfffffffc)
+print(hex(sm.get()))
+sm.put(0xfffffffd)
+print(hex(sm.get()))
+sm.put(0xfffffffe)
+print(hex(sm.get()))
+sm.put(0xffffffff)
+print(hex(sm.get()))
 sm.active(0)
+machine.reset()
